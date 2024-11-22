67643dfb3137eb727ba582d3c9acdbef5983cfd7
Andi Triyadi <sheratan17@users.noreply.github.com>
2024-11-22 17:16:54 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -60,23 +60,23 @@ pip install fastapi uvicorn
 # Aktifkan quota di /home
 grep -q "usrjquota=aquota.user,grpjquota=aquota.group,jqfmt=vfsv1" /etc/fstab
 
-#partition=$(df /home | awk 'NR==2 {print $1}')
-#umount /home
-#tune2fs -O quota $partition
-#mount -a
+partition=$(df /home | awk 'NR==2 {print $1}')
+umount /home
+tune2fs -O quota $partition
+mount /home
 #quotaon -va
 
-if [ $? -eq 0 ]; then
-	echo "/etc/fstab terdeteksi sudah ada quota."
-	else
-	line=$(grep "^UUID=.* /home " /etc/fstab)
-	new_line=$(echo "$line" | sed 's/defaults/&,usrjquota=aquota.user,grpjquota=aquota.group,jqfmt=vfsv1/')
-	sed -i "s|$line|$new_line|" /etc/fstab
-	mount -o remount /home
-	quotacheck -cugm /home
-	quotaon -v /home
-	quotaon -ap
-fi
+#if [ $? -eq 0 ]; then
+#	echo "/etc/fstab terdeteksi sudah ada quota."
+#	else
+#	line=$(grep "^UUID=.* /home " /etc/fstab)
+#	new_line=$(echo "$line" | sed 's/defaults/&,usrjquota=aquota.user,grpjquota=aquota.group,jqfmt=vfsv1/')
+#	sed -i "s|$line|$new_line|" /etc/fstab
+#	mount -o remount /home
+#	quotacheck -cugm /home
+#	quotaon -v /home
+#	quotaon -ap
+#fi
 
 # Setup firewall
 sed -i "s/AllowZoneDrifting=yes/AllowZoneDrifting=no/g" /etc/firewalld/firewalld.conf
