bcd26c19ad4bfca89538ed58eb8b2fd1f227210e
Andi Triyadi <sheratan17@users.noreply.github.com>
2025-01-08 13:52:43 +0700

    tambah port 3306

--- deploy.sh
+++ deploy.sh
@@ -460,6 +460,7 @@ UPDATE global_variables SET variable_value='monitor' WHERE variable_name='mysql-
 UPDATE global_variables SET variable_value='Monitor123' WHERE variable_name='mysql-monitor_password';
 UPDATE global_variables SET variable_value='2000' WHERE variable_name IN ('mysql-monitor_connect_interval','mysql-monitor_ping_interval','mysql-monitor_read_only_interval');
 SELECT * FROM global_variables WHERE variable_name LIKE 'mysql-monitor_%';
+UPDATE global_variables SET variable_value='0.0.0.0:3306;/tmp/proxysql.sock' WHERE variable_name='mysql-interfaces';
 LOAD MYSQL VARIABLES TO RUNTIME;
 SAVE MYSQL VARIABLES TO DISK;
 LOAD MYSQL SERVERS TO RUNTIME;
